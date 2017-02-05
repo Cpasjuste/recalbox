84c829f7577948ef936ec33f9bebd70275f51097
Subs <frog2wah@gmail.com>
2017-02-05 13:08:41 +0100

    Advancemame: bump to 3.2 + remove annoying folders

--- package/advancemame/advancemame.mk
+++ package/advancemame/advancemame.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ADVANCEMAME_VERSION = v3.1-rc
+ADVANCEMAME_VERSION = v3.2
 ADVANCEMAME_SITE = $(call github,amadvance,advancemame,$(ADVANCEMAME_VERSION))
 ADVANCEMAME_LICENSE = GPLv2
 
@@ -24,20 +24,21 @@ ADVANCEMAME_CONF_OPTS += \
 	--enable-pthreads \
 	--prefix=$(TARGET_DIR)/usr \
 	--disable-oss \
-	--with-emu=mame
+	--disable-mraw \
+	--disable-mevent
 	
-ifeq ($(BR2_PACKAGE_SDL),y)
+ifeq ($(BR2_PACKAGE_SDL2),y)
 	ADVANCEMAME_DEPENDENCIES += sdl
 	ADVANCEMAME_CONF_OPTS += \
-		--enable-sdl \
-		--with-sdl-prefix=$(STAGING_DIR)/usr \
+		--enable-sdl2 \
+		--with-sdl2-prefix=$(STAGING_DIR)/usr \
 		--enable-jsdl \
 		--enable-ksdl \
-		--enable-msdl
+		--disable-msdl
 else
 	ADVANCEMAME_CONF_OPTS += \
 		--disable-jsdl \
-		--disable-ksdl \
+		--disable-ksdl
 		--disable-msdl
 endif
 
@@ -56,8 +57,15 @@ else
 	ADVANCEMAME_CONF_OPTS += --disable-freetype
 endif
 
-ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
-	ADVANCEMAME_DEPENDENCIES += rpi-userland
+define ADVANCEMAME_ADD_PI_LINK
+	$(SED) "s/^CONF_LIBS=.*/& -lbcm_host -lvcos -lvchostif/" $(@D)/Makefile ;
+endef
+
+ifeq ($(BR2_PACKAGE_RPI_FIRMWARE),y)
+	ADVANCEMAME_DEPENDENCIES += rpi-firmware
+	ADVANCEMAME_CONF_OPTS += --enable-vc \
+		--with-vc-prefix=$(STAGING_DIR)/usr
+	ADVANCEMAME_POST_CONFIGURE_HOOKS += ADVANCEMAME_ADD_PI_LINK
 endif
 
 #ifeq ($(BR2_PACKAGE_ADVANCEMAME_MESS),y)
