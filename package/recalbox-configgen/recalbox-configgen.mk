ccc44a0f42c1a2818a0124da9c538cc6c6b4ed7e
Subs <frog2wah@gmail.com>
2017-08-17 15:46:36 +0200

    configgen: bump for reicast save dir

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = f348e77472f06130dcda3d7b8763f0ecda1047ce
+RECALBOX_CONFIGGEN_VERSION = 870e1fff525b86089846ad8e95c9e472b5309575
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
