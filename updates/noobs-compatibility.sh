5879873dea94280e8adf1b51ea6cd97180ff6612
digitalLumberjack <digitallumberjack@gmail.com>
2017-08-20 00:54:27 +0200

    added noobs compatibility wiaiting for os_list_v3.json is up to date

--- updates/noobs-compatibility.sh
+++ updates/noobs-compatibility.sh
@@ -2,6 +2,7 @@
 
 UPGRADE_DIR="/usr/share/nginx/html/v1/upgrade"
 NOOBS_DIR="/usr/share/nginx/html/v1/noobs"
+OLD_NOOBS_DIR="/usr/share/nginx/html/noobs"
 
 for arch in $(ls "${UPGRADE_DIR}" | grep rpi); do
   echo "Creating directory ${NOOBS_DIR}/${arch}"
@@ -11,3 +12,7 @@ for arch in $(ls "${UPGRADE_DIR}" | grep rpi); do
     ln -s "${UPGRADE_DIR}/${arch}/${file}" "${NOOBS_DIR}/${arch}/${file}"
   done
 done
+
+# Only usefull until noobs did not change os_list_v3.json
+echo "Linking directory ${NOOBS_DIR} to ${OLD_NOOBS_DIR}"
+ln -s "${NOOBS_DIR}" "${OLD_NOOBS_DIR}"
