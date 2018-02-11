497205198b7b4dc79b4e959782e694b84eff1e3d
OyyoDams <oyyodams@oyyodams.com>
2018-02-11 17:07:27 +0100

    Bump FBA Libretro

--- package/libretro-fba/libretro-fba.mk
+++ package/libretro-fba/libretro-fba.mk
@@ -3,7 +3,7 @@
 # FBA
 #
 ################################################################################
-LIBRETRO_FBA_VERSION = 2021f2d811982d79c90b6d3d47a1acc6d9ffa580
+LIBRETRO_FBA_VERSION = e54b9c31207ad3798b28dca80756a7cf8573256f
 LIBRETRO_FBA_SITE = $(call github,libretro,fbalpha,$(LIBRETRO_FBA_VERSION))
 
 define LIBRETRO_FBA_BUILD_CMDS
