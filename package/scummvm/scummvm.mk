b3571aad93d98fe4fd0653a4303fcdec049e7fe4
Laurent Merckx <laurent-merckx@skynet.be>
2018-02-15 21:26:09 +0100

    ScummVM: Enable MT32 emulation

--- package/scummvm/scummvm.mk
+++ package/scummvm/scummvm.mk
@@ -18,7 +18,7 @@ ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 endif
 
 SCUMMVM_CONF_ENV += RANLIB="$(TARGET_RANLIB)" STRIP="$(TARGET_STRIP)" AR="$(TARGET_AR) cru" AS="$(TARGET_AS)"
-SCUMMVM_CONF_OPTS += --enable-opengl --disable-debug --enable-optimizations --disable-mt32emu --enable-flac --enable-mad --enable-vorbis --disable-tremor \
+SCUMMVM_CONF_OPTS += --enable-opengl --disable-debug --enable-optimizations --enable-mt32emu --enable-flac --enable-mad --enable-vorbis --disable-tremor \
 		--enable-fluidsynth --disable-taskbar --disable-timidity --disable-alsa --enable-vkeybd --enable-keymapper --disable-eventrecorder \
 		--prefix=/usr --with-sdl-prefix="$(STAGING_DIR)/usr/bin/" --with-freetype2-prefix="$(STAGING_DIR)/usr/bin/" \
 		--enable-all-engines --enable-release
