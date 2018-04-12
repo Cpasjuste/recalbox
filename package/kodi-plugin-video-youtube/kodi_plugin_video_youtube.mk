4338127a68ab610caa80eff00e5f63e078b77d0d
Yann MORERE <yann.morere@gmail.com>
2018-04-12 13:09:35 +0000

    Update kodi_plugin_video_youtube.mk

--- package/kodi-plugin-video-youtube/kodi_plugin_video_youtube.mk
+++ package/kodi-plugin-video-youtube/kodi_plugin_video_youtube.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PLUGIN_VIDEO_YOUTUBE_VERSION = 5.4.0
+KODI_PLUGIN_VIDEO_YOUTUBE_VERSION = 5.4.6
 KODI_PLUGIN_VIDEO_YOUTUBE_SOURCE = plugin.video.youtube-$(KODI_PLUGIN_VIDEO_YOUTUBE_VERSION).zip
 KODI_PLUGIN_VIDEO_YOUTUBE_SITE = http://mirrors.kodi.tv/addons/jarvis/plugin.video.youtube
 KODI_PLUGIN_VIDEO_YOUTUBE_PLUGINNAME=plugin.video.youtube
