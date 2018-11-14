f8def35a3319a3d62f7b6158126a8bc32fb7af1f
OyyoDams <oyyodams@oyyodams.com>
2018-11-14 17:25:40 +0100

    FBA libretro source update

--- package/libretro-fba/libretro-fba.mk
+++ package/libretro-fba/libretro-fba.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 LIBRETRO_FBA_VERSION = d48197f98fe82f96bcec083cac6c582b5bec253a
-LIBRETRO_FBA_SITE = $(call github,libretro,fbalpha,$(LIBRETRO_FBA_VERSION))
+LIBRETRO_FBA_SITE = $(call github,libretro,fbalpha2018,$(LIBRETRO_FBA_VERSION))
 
 define LIBRETRO_FBA_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f makefile.libretro platform="$(LIBRETRO_BOARD)"
