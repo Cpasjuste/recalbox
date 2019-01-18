1b4822abea26cf98caeb9de66414ebad2297345a
Bkg2k <bkg2k9@gmail.com>
2019-01-18 08:28:22 +0100

    Moved to /usr/share/simcoupe

--- package/simcoupe/simcoupe.mk
+++ package/simcoupe/simcoupe.mk
@@ -7,16 +7,19 @@ SIMCOUPE_VERSION = 3be5b80b14143b608f40cbecc4668da76701b448
 SIMCOUPE_SITE = $(call github,simonowen,simcoupe,$(SIMCOUPE_VERSION))
 SIMCOUPE_DEPENDENCIES = sdl2
 
+SIMCOUPE_BIOS_AND_RESOURCES = /usr/share/simcoupe
+
 define SIMCOUPE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/simcoupe \
 		$(TARGET_DIR)/usr/bin/simcoupe
-	mkdir -p $(TARGET_DIR)/recalbox/share_init/bios/samcoupe
-	cp -R $(@D)/Resource/** $(TARGET_DIR)/recalbox/share_init/bios/samcoupe
+	mkdir -p $(TARGET_DIR)$(SIMCOUPE_BIOS_AND_RESOURCES)
+	cp -R $(@D)/Resource/** $(TARGET_DIR)$(SIMCOUPE_BIOS_AND_RESOURCES)
 endef
 
 define SIMCOUPE_POST_EXTRACT_FIX_SDL2_PATH
-    @echo CHANGING CMakeList.txt
-    /bin/sed -i -E -e "s|set\(RESOURCE_DIR \\$$\{CMAKE_INSTALL_PREFIX\}/share/\\$$\{PROJECT_NAME\}\)|set(RESOURCE_DIR /recalbox/share_init/bios/samcoupe)|g" $(@D)/CMakeLists.txt
+    # Change emulator resource folder
+    /bin/sed -i -E -e "s|set\(RESOURCE_DIR \\$$\{CMAKE_INSTALL_PREFIX\}/share/\\$$\{PROJECT_NAME\}\)|set(RESOURCE_DIR $(SIMCOUPE_BIOS_AND_RESOURCES))|g" $(@D)/CMakeLists.txt
+    # DOS2UNIX Joystick.cpp - patch system does not support different line endings
     /bin/sed -i -E -e "s|\r$$||g" $(@D)/Base/Joystick.cpp
 endef
  
@@ -27,7 +30,7 @@ SIMCOUPE_DEPENDENCIES += libgl
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
-SIMCOUPE += libgles
+SIMCOUPE_DEPENDENCIES += libgles
 endif
 
 $(eval $(cmake-package))
