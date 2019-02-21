5bf73f5aecaafcd070958cd94844eee7ae00d0d1
Bkg2k <bkg2k9@gmail.com>
2019-02-21 14:49:25 +0100

    Bump conffiggen to add '0x' disk supports to Amiberry

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = bb7c2f9202a1e93de4f75c25e276b4a179de9802
+RECALBOX_CONFIGGEN_VERSION = 89046bbd7f8fd1ac0b7f9bd242404f0d580a0fb8
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
