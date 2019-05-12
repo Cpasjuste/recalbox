ddef0ce291e8e7ec18a148ec486a80d765b5a4a9
Bkg2k <bkg2k9@gmail.com>
2019-05-12 19:06:25 +0200

    feat(core): add mame2016 core

---
+++ package/libretro-mame2016/libretro-mame2016.mk
@@ -0,0 +1,44 @@
+################################################################################
+#
+# MAME2016
+#
+################################################################################
+
+LIBRETRO_MAME2016_VERSION = e06d731644217f46bf5a7613222632d41a327f93
+LIBRETRO_MAME2016_SITE = $(call github,libretro,mame2016-libretro,$(LIBRETRO_MAME2016_VERSION))
+LIBRETRO_MAME2016_LICENSE = MAME
+
+LIBRETRO_MAME2016_OPTS = PTR64=0
+# x86_64
+ifeq ($(BR2_x86_64),y)
+LIBRETRO_MAME2016_OPTS = PTR64=1
+endif
+
+LIBRETRO_MAME2016_OPTS += platform="unix"
+LIBRETRO_MAME2016_OPTS += LIBRETRO_CPU="$(BR2_ARCH)"
+LIBRETRO_MAME2016_OPTS += LIBRETRO_OS="unix"
+LIBRETRO_MAME2016_OPTS += CONFIG="libretro"
+LIBRETRO_MAME2016_OPTS += OSD="retro"
+LIBRETRO_MAME2016_OPTS += ARCHOPTS="$(COMPILER_COMMONS_BOARD_OPTIONS)"
+LIBRETRO_MAME2016_OPTS += OPTIMIZE=3
+LIBRETRO_MAME2016_OPTS += LTO=1
+LIBRETRO_MAME2016_OPTS += NOWERROR=1
+LIBRETRO_MAME2016_OPTS += PYTHON_EXECUTABLE=python2
+LIBRETRO_MAME2016_OPTS += REGENIE=1
+LIBRETRO_MAME2016_OPTS += VERBOSE=1
+LIBRETRO_MAME2016_OPTS += SUBTARGET=arcade
+
+define LIBRETRO_MAME2016_BUILD_CMDS
+	mkdir -p $(@D)/build/gmake/libretro/obj/x64/libretro/src/osd/retro
+	mkdir -p $(@D)/3rdparty/genie/build/gmake.linux/obj/Release/src/host
+	$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_CC)-ar" -C $(@D)/ -f makefile $(LIBRETRO_MAME2016_OPTS)
+endef
+
+define LIBRETRO_MAME2016_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/mamearcade2016_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/mame2016_libretro.so
+	mkdir -p $(TARGET_DIR)/recalbox/share_init/bios/mame2016/samples
+	cp -R $(@D)/metadata/* $(TARGET_DIR)/recalbox/share_init/bios/mame2016
+endef
+
+$(eval $(generic-package))
