a4336da339c55b97f1bfe518aff4e75acca7c503
OyyoDams <oyyodams@recalbox.com>
2019-05-15 07:32:24 +0200

    build: odroid xu4 kernel bump

--- package/uboot-xu4/uboot-xu4.mk
+++ package/uboot-xu4/uboot-xu4.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-UBOOT_XU4_VERSION = 8a47b2602ed2e348ed2a57dc2cd874c4605c538d
+UBOOT_XU4_VERSION = 88af53fbcef8386cb4d5f04c19f4b2bcb69e90ca
 UBOOT_XU4_SITE = $(call github,hardkernel,u-boot,$(UBOOT_XU4_VERSION))
 UBOOT_XU4_INSTALL_TARGET = NO
 UBOOT_XU4_INSTALL_IMAGES = YES
 
 define UBOOT_XU4_INSTALL_IMAGES_CMDS
-	cp -dpf $(@D)/sd_fuse/hardkernel/u-boot.bin.hardkernel \
-		$(@D)/sd_fuse/hardkernel/bl1.bin.hardkernel \
-		$(@D)/sd_fuse/hardkernel/bl2.bin.hardkernel \
-		$(@D)/sd_fuse/hardkernel/tzsw.bin.hardkernel \
+	cp -dpf $(@D)/sd_fuse/u-boot.bin.hardkernel \
+		$(@D)/sd_fuse/bl1.bin.hardkernel \
+		$(@D)/sd_fuse/bl2.bin.hardkernel.720k_uboot \
+		$(@D)/sd_fuse/tzsw.bin.hardkernel \
 		$(BINARIES_DIR)
 endef
 $(eval $(generic-package))
