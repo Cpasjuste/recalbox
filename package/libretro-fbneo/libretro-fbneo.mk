80d65935b2597636de7e750bb068705bc151fcd5
paradadf <paradad@outlook.com>
2019-06-10 19:15:14 +0000

    chore(fbneo): right samples location and kinst

--- package/libretro-fbneo/libretro-fbneo.mk
+++ package/libretro-fbneo/libretro-fbneo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_FBNEO_VERSION = 6df0149698c44815463077a5083bdc5787d9a5b4
+LIBRETRO_FBNEO_VERSION = abc93464d1291f6994aa80143d549e4ef0b331fe
 LIBRETRO_FBNEO_SITE = $(call github,libretro,FBNeo,$(LIBRETRO_FBNEO_VERSION))
 
 ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
@@ -17,6 +17,10 @@ ifeq ($(BR2_arm),y)
 LIBRETRO_FBNEO_OPTIONS += USE_CYCLONE=1
 endif
 
+ifeq ($(BR2_x86_64),y)
+LIBRETRO_FBNEO_OPTIONS += USE_X64_DRC=1
+endif
+
 define LIBRETRO_FBNEO_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
@@ -27,12 +31,12 @@ endef
 define LIBRETRO_FBNEO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/src/burner/libretro/fbneo_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/fbneo_libretro.so
-	mkdir -p $(TARGET_DIR)/recalbox/share_init/bios/fbn/samples
+	mkdir -p $(TARGET_DIR)/recalbox/share_init/bios/fbneo/samples
 	cp "$(@D)/dats/FinalBurn Neo (ClrMame Pro XML, Arcade only).dat" \
-		$(TARGET_DIR)/recalbox/share_init/bios/fbn
+		$(TARGET_DIR)/recalbox/share_init/bios/fbneo
 	cp "$(@D)/dats/FinalBurn Neo (ClrMame Pro XML, Neogeo only).dat" \
-		$(TARGET_DIR)/recalbox/share_init/bios/fbn
-	cp -R $(@D)/metadata/* $(TARGET_DIR)/recalbox/share_init/bios/fbn
+		$(TARGET_DIR)/recalbox/share_init/bios/fbneo
+	cp -R $(@D)/metadata/* $(TARGET_DIR)/recalbox/share_init/bios/fbneo
 endef
 
 $(eval $(generic-package))
