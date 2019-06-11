c2bcd9d60e85e6006b91be84ba49210236bfecf0
Bkg2k <bkg2k9@gmail.com>
2019-06-11 19:59:17 +0200

    fix(package): fix names

--- package/pwm-audio-pizero-gpicase/pwm-audio-pizero-gpicase.mk
+++ package/pwm-audio-pizero-gpicase/pwm-audio-pizero-gpicase.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PWM_AUDIO_PI_ZERO_VERSION = 66cab631803aef8495616ce1460881c764be38ca
-PWM_AUDIO_PI_ZERO_SITE = $(call github,ian57,pwm-audio-pizero-gpicase,$(PWM_AUDIO_PI_ZERO_VERSION))
-PWM_AUDIO_PI_ZERO_DEPENDENCIES += rpi-firmware
+PWM_AUDIO_PIZERO_GPICASE_VERSION = 66cab631803aef8495616ce1460881c764be38ca
+PWM_AUDIO_PIZERO_GPICASE_SITE = $(call github,ian57,pwm-audio-pizero-gpicase,$(PWM_AUDIO_PIZERO_GPICASE_VERSION))
+PWM_AUDIO_PIZERO_GPICASE_DEPENDENCIES += rpi-firmware
 
-define PWM_AUDIO_PI_ZERO_INSTALL_TARGET_CMDS
+define PWM_AUDIO_PIZERO_GPICASE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/pwm-audio-pizero-gpicase-overlay.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/pwm-audio-pizero-gpicase.dtbo
 	$(INSTALL) -D -m 0644 $(@D)/pwm-audio-pizero-gpicase-overlay.dts $(BINARIES_DIR)/rpi-firmware/overlays/pwm-audio-pizero-gpicase.dts
 endef
