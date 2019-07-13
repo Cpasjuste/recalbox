4d73481e0911f59d7546721f0a5c19adc9be4a7d
Bkg2k <bkg2k9@gmail.com>
2019-07-13 09:07:27 +0200

    fix(sameboy): move rgbds from host to stagingdir

--- package/rgbds/rgbds.mk
+++ package/rgbds/rgbds.mk
@@ -7,16 +7,18 @@
 RGBDS_VERSION = v0.3.8
 RGBDS_SITE = $(call github,rednex,rgbds,$(RGBDS_VERSION))
 RGBDS_DEPENDENCIES = libpng
+RGBDS_INSTALL_TARGET = NO
+RGBDS_INSTALL_STAGING = YES
 
 define RGBDS_BUILD_CMDS
 	$(MAKE) -C $(@D)
 endef
 
-define RGBDS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/rgbasm $(HOST_DIR)/usr/bin/
-	$(INSTALL) -D $(@D)/rgbfix $(HOST_DIR)/usr/bin/
-	$(INSTALL) -D $(@D)/rgbgfx $(HOST_DIR)/usr/bin/
-	$(INSTALL) -D $(@D)/rgblink $(HOST_DIR)/usr/bin/
+define RGBDS_INSTALL_STAGING_CMDS
+	$(INSTALL) -D $(@D)/rgbasm $(STAGING_DIR)/usr/bin/
+	$(INSTALL) -D $(@D)/rgbfix $(STAGING_DIR)/usr/bin/
+	$(INSTALL) -D $(@D)/rgbgfx $(STAGING_DIR)/usr/bin/
+	$(INSTALL) -D $(@D)/rgblink $(STAGING_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
