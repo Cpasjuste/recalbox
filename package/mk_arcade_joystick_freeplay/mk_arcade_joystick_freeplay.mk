f3642ee74057ec30ef654c8b3ffe712ec95e7960
Cpasjuste <cpasjuste@gmail.com>
2019-08-30 15:44:39 +0200

    freeplay: fix gpio inputs (mk_arcade_joystick)

--- package/mk_arcade_joystick_freeplay/mk_arcade_joystick_freeplay.mk
+++ package/mk_arcade_joystick_freeplay/mk_arcade_joystick_freeplay.mk
@@ -8,6 +8,18 @@ MK_ARCADE_JOYSTICK_FREEPLAY_VERSION = 4692beeb5490c6d061de3cacf84ac442bd35b3a3
 MK_ARCADE_JOYSTICK_FREEPLAY_SITE = $(call github,Cpasjuste,mk_arcade_joystick_rpi,$(MK_ARCADE_JOYSTICK_FREEPLAY_VERSION))
 MK_ARCADE_JOYSTICK_FREEPLAY_DEPENDENCIES = linux
 
+# Needed beacause can't pass cflags to cc
+define MK_ARCADE_JOYSTICK_FREEPLAY_RPI2_HOOK
+	$(SED) "s/#define PERI_BASE        0x20000000/#define PERI_BASE        0x3F000000/g" $(@D)/mk_arcade_joystick_rpi.c
+endef
+
+ifeq ($(BR2_cortex_a7),y)
+MK_ARCADE_JOYSTICK_FREEPLAY_PRE_CONFIGURE_HOOKS += MK_ARCADE_JOYSTICK_FREEPLAY_RPI2_HOOK
+endif
+ifeq ($(BR2_cortex_a53),y)
+MK_ARCADE_JOYSTICK_FREEPLAY_PRE_CONFIGURE_HOOKS += MK_ARCADE_JOYSTICK_FREEPLAY_RPI2_HOOK
+endif
+
 define MK_ARCADE_JOYSTICK_FREEPLAY_MAKE_HOOK
 	cp $(@D)/Makefile.cross $(@D)/Makefile
 endef
@@ -19,6 +31,7 @@ endef
 
 define MK_ARCADE_JOYSTICK_FREEPLAY_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) modules_install
+	$(INSTALL) -D -m 0644 $(@D)/mk_arcade_joystick.conf $(TARGET_DIR)/etc/modprobe.d/mk_arcade_joystick.conf
 endef
 
 $(eval $(generic-package))
