180ea23db5f417b8e8353bdbfe902b8230213391
Cpasjuste <cpasjuste@gmail.com>
2019-08-31 11:25:10 +0200

    freeplay: use freeplay-support package for freeplay stuff

--- package/freeplay-support/freeplay-support.mk
+++ package/freeplay-support/freeplay-support.mk
@@ -5,12 +5,12 @@
 ################################################################################
 
 FREEPLAY_SUPPORT_VERSION = b5197174aded98d903de9d3ae12359a3bc2f633d
-FREEPLAY_SUPPORT_SITE = $(call github,Cpasjuste,freeplay-support,$(FREEPLAY_SUPPORT_VERSION))
+FREEPLAY_SUPPORT_SOURCE = 
+#FREEPLAY_SUPPORT_SITE = $(call github,Cpasjuste,freeplay-support,$(FREEPLAY_SUPPORT_VERSION))
 FREEPLAY_SUPPORT_DEPENDENCIES += rpi-firmware
 
 define FREEPLAY_SUPPORT_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/config.txt $(BINARIES_DIR)/rpi-firmware/config.txt
+	$(INSTALL) -D -m 0644 $(FREEPLAY_SUPPORT_PKGDIR)/config.txt $(BINARIES_DIR)/rpi-firmware/config.txt
 endef
 
 $(eval $(generic-package))
-
