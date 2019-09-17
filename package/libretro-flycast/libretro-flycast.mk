b37f6a778ac4f93b2273dd725cc736928767a531
paradadf <paradad@outlook.com>
2019-09-17 13:15:07 +0000

    Resolve "Fix naomi and atomiswave"

--- package/libretro-flycast/libretro-flycast.mk
+++ package/libretro-flycast/libretro-flycast.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_FLYCAST_VERSION = 652e1af12aa6b359a3eccb57b2894bf001b996f8
+LIBRETRO_FLYCAST_VERSION = f54976ccc3f6ba4bf6e62fadaad114fc381e55f8
 LIBRETRO_FLYCAST_SITE = $(call github,libretro,flycast,$(LIBRETRO_FLYCAST_VERSION))
 LIBRETRO_FLYCAST_LICENSE = GPLv2
 
